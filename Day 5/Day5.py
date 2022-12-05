22cdd5bfc2550fa51c67e5af02c8890690d3157d
Anton Sandberg <antsandb@student.chalmers.se>
2022-12-05 12:47:35 +0100

    ok

--- Day 5/Day5.py
+++ Day 5/Day5.py
@@ -22,7 +22,7 @@ def process_data(FILENAME):
 
 
 def main():
-    FILENAME = 'Day 5\input.txt'
+    FILENAME = 'input.txt'
     stacks, moves = process_data(FILENAME)
     stacks_2, _ = process_data(FILENAME)
     # Part 1
@@ -31,7 +31,6 @@ def main():
         for _ in range(int(n_of_moves)):
             stacks[int(destination)].append(stacks[int(location)].pop())
     
-    #on_top_of = []
     part_1 = "".join([c[-1] for c in stacks.values()])
     print(f'Day 5 part 1: {part_1}')
 
@@ -45,9 +44,9 @@ def main():
 
         stacks_2[int(destination)] += tmp_list[::-1]
 
-
     part_2 = "".join([c[-1] for c in stacks_2.values()])
     print(f'Day 5 part 2: {part_2}')
+
 
 if __name__ == '__main__':
     t1 = time.perf_counter()
